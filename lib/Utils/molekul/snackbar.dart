4d976fb50a33c75cba55e488efe3b39d2836d68e
dhohirpradana <dhohirkudus@gmail.com>
2021-05-01 23:46:33 +0700

    Migrate to null safety

--- lib/Utils/molekul/snackbar.dart
+++ lib/Utils/molekul/snackbar.dart
@@ -1,18 +1,10 @@
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 void showFlushBar(BuildContext context) {
-  Flushbar(
-    isDismissible: true,
-    barBlur: 0.5,
-    title: 'Information',
-    message: 'Tidak boleh kurang dari 2 data Alternatif',
-    icon: Icon(
-      Icons.info_outline_rounded,
-      size: 28,
-      color: Colors.blue.shade300,
+  ScaffoldMessenger.of(context).showSnackBar(
+    const SnackBar(
+      elevation: 2,
+      content: Text('Tidak boleh kurang dari 2 data Alternatif'),
     ),
-    leftBarIndicatorColor: Colors.blue.shade300,
-    duration: Duration(milliseconds: 1700),
-  )..show(context);
+  );
 }
