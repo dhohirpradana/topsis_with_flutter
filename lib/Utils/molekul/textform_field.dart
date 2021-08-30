d6420ad817f5997822b773b9264ae9b941f8b310
dhohirpradana <dhohirkudus@gmail.com>
2021-08-30 21:25:07 +0700

    fix

--- lib/Utils/molekul/textform_field.dart
+++ lib/Utils/molekul/textform_field.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:topsis_tanaman_hias/Pages/alternatif_page.dart';
 import 'package:topsis_tanaman_hias/Utils/List/topsis_list.dart';
 import 'package:topsis_tanaman_hias/Utils/atom/hint_text.dart';
 import 'package:topsis_tanaman_hias/Utils/atom/label_text.dart';
@@ -17,17 +15,12 @@ class TextFormFieldWidget extends StatefulWidget {
 }
 
 class _TextFormFieldWidgetState extends State<TextFormFieldWidget> {
-  List<TextEditingController> _controller = [
-    TextEditingController(),
-    TextEditingController(),
-    TextEditingController(),
-    TextEditingController(),
-    TextEditingController()
-  ];
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final i = widget.i;
     final index = widget.index;
+    List tinggi = ["1 - 10", "11 - 20", "21 - 30"];
     var params = <String>[
       (index == 1)
           ? 'Hijau Muda'
@@ -56,114 +49,149 @@ class _TextFormFieldWidgetState extends State<TextFormFieldWidget> {
     String? _dropDownValue = (index != 0)
         ? (topsis[i]['c' + (index + 1).toString()] != 0)
             ? params[topsis[i]['c' + (index + 1).toString()] - 1]
-            : params[0]
+            : null
+        : null;
+
+    String? _dropDownTinggiValue = (topsis[i]['c1'.toString()] != 0)
+        ? topsis[i]['c1'.toString()].toString()
         : null;
 
     return Container(
-        margin: EdgeInsets.only(left: 5, right: 5, top: 7),
-        child: (index == 0)
-            ? TextFormField(
-                inputFormatters: [
-                    LengthLimitingTextInputFormatter(2),
-                  ],
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    if (value == '0') {
-                      return 'Nilai tidak boleh 0(nol)';
-                    }
-                    return 'Harus diisi';
-                  }
-                  return null;
-                },
-                key: Key(index.toString()),
-                showCursor: true,
-                readOnly: false,
-                controller: _controller[i]
-                  ..text = ((topsis[i]['c' + (index + 1).toString()])
-                              .toString() ==
-                          '0')
-                      ? ''
-                      : (topsis[i]['c' + (index + 1).toString()]).toString(),
-                onChanged: (value) => (value == '')
-                    ? topsis[i]['c' + (index + 1).toString()] = 0
-                    : topsis[i]['c' + (index + 1).toString()] =
-                        int.parse(value),
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1.0),
+      margin: EdgeInsets.only(left: 5, right: 5, top: 7),
+      child: (index == 0)
+          ? Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.black54,
+                      width: 1,
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54, width: 1.0),
-                    ),
-                    hintText: hintText(index),
-                    labelText: labelText(index)))
-            : Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.circular(3),
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Colors.black54,
-                        width: 1,
-                      ),
-                    ),
-                    height: 60,
-                    width: MediaQuery.of(context).size.width - 10,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Theme(
-                        data: Theme.of(context)
-                            .copyWith(canvasColor: Colors.green[50]),
-                        child: DropdownButton(
-                          elevation: 0,
-                          hint: _dropDownValue == null
-                              ? Text(labelText(index))
-                              : Text(
-                                  _dropDownValue,
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                          isExpanded: true,
-                          iconSize: 30.0,
-                          style: TextStyle(color: Colors.black),
-                          items: params.map(
-                            (val) {
-                              return DropdownMenuItem<String>(
-                                value: val,
-                                child: Text(val),
-                              );
-                            },
-                          ).toList(),
-                          onChanged: (val) {
-                            FocusScope.of(context)
-                                .requestFocus(new FocusNode());
-                            setState(
-                              () {
-                                topsis[i]['c' + (index + 1).toString()] =
-                                    params.indexOf(val.toString()) + 1;
-                                print(topsis[i]['c' + (index + 1).toString()]);
-                                _dropDownValue = val.toString();
-                              },
+                  ),
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Theme(
+                      data: Theme.of(context)
+                          .copyWith(canvasColor: Colors.green[50]),
+                      child: DropdownButton(
+                        elevation: 0,
+                        hint: _dropDownTinggiValue == null
+                            ? Text('0 cm')
+                            : Text(
+                                _dropDownTinggiValue + ' cm',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                        isExpanded: true,
+                        iconSize: 30.0,
+                        style: TextStyle(color: Colors.black),
+                        items: tinggi.map(
+                          (val) {
+                            return DropdownMenuItem<String>(
+                              value: val,
+                              child: Text(val),
                             );
                           },
-                        ),
+                        ).toList(),
+                        onChanged: (val) {
+                          FocusScope.of(context).requestFocus(new FocusNode());
+                          setState(
+                            () {
+                              topsis[i]['c1'.toString()] =
+                                  tinggi.indexOf(val.toString()) + 1;
+                              // int.parse(val.toString());
+                              _dropDownTinggiValue = val.toString();
+                            },
+                          );
+                        },
                       ),
                     ),
                   ),
-                  Positioned(
-                      left: 8,
-                      child: Container(
-                          padding:
-                              EdgeInsets.only(bottom: 0, left: 5, right: 5),
-                          color: Colors.white,
-                          child: Text(
-                            labelText(index),
-                            style: TextStyle(color: Colors.black54),
-                          ))),
-                ],
-              ));
+                ),
+                Positioned(
+                  left: 8,
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 0, left: 5, right: 5),
+                    color: Colors.white,
+                    child: Text(
+                      labelText(index),
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.black54,
+                      width: 1,
+                    ),
+                  ),
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Theme(
+                      data: Theme.of(context)
+                          .copyWith(canvasColor: Colors.green[50]),
+                      child: DropdownButton(
+                        elevation: 0,
+                        hint: _dropDownValue == null
+                            ? Text('-- Pilih --')
+                            : Text(
+                                _dropDownValue,
+                                style: TextStyle(color: Colors.black),
+                              ),
+                        isExpanded: true,
+                        iconSize: 30.0,
+                        style: TextStyle(color: Colors.black),
+                        items: params.map(
+                          (val) {
+                            return DropdownMenuItem<String>(
+                              value: val,
+                              child: Text(val),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (val) {
+                          FocusScope.of(context).requestFocus(new FocusNode());
+                          setState(
+                            () {
+                              topsis[i]['c' + (index + 1).toString()] =
+                                  params.indexOf(val.toString()) + 1;
+                              print(topsis[i]['c' + (index + 1).toString()]);
+                              _dropDownValue = val.toString();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 8,
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 0, left: 5, right: 5),
+                    color: Colors.white,
+                    child: Text(
+                      labelText(index),
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
   }
 }
 
